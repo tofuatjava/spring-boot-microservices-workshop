f5b77de0eb6f24f948346b4b6bdd3a668898fe93
U407008 <thomas.fuerer@voestalpine.com>
2019-10-30 14:15:17 +0100

    remove eureka

--- ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/RatingsDataServiceApplication.java
+++ ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/RatingsDataServiceApplication.java
@@ -2,15 +2,12 @@ package io.javabrains.ratingsdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class RatingsDataServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RatingsDataServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RatingsDataServiceApplication.class, args);
+    }
 
 }
-
